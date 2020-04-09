cd5859865fb24b8888769d046b0b783c6484363f
msrocka <michael.srocka@gmail.com>
2020-04-09 22:46:08 +0200

    remove the old stock routes

--- data_stocks.go
+++ data_stocks.go
@@ -8,23 +8,23 @@ import (
 
 func (s *server) handleGetDataStocks() http.HandlerFunc {
 
-	type StockItem struct {
+	type item struct {
 		IsRoot    bool   `xml:"root,attr"`
 		ID        string `xml:"uuid"`
 		ShortName string `xml:"shortName"`
 		Name      string `xml:"name"`
 	}
 
-	type Response struct {
-		XMLName xml.Name    `xml:"http://www.ilcd-network.org/ILCD/ServiceAPI dataStockList"`
-		Items   []StockItem `xml:"dataStock"`
+	type response struct {
+		XMLName xml.Name `xml:"http://www.ilcd-network.org/ILCD/ServiceAPI dataStockList"`
+		Items   []item   `xml:"dataStock"`
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		resp := Response{}
+		resp := response{}
 		for _, stock := range s.dir.dataStocks {
 			name := filepath.Base(stock.dir)
-			resp.Items = append(resp.Items, StockItem{
+			resp.Items = append(resp.Items, item{
 				IsRoot:    name == "root",
 				ID:        stock.uid,
 				ShortName: name,
